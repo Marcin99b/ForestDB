6112eea04181e8ee8f1fd721ffade21b6673795a
Marcin99b <marcin.bigos99@gmail.com>
2024-06-27 00:12:32 +0200

    download worker

--- src/ForestDB/DataCollector/Program.cs
+++ src/ForestDB/DataCollector/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+var httpClient = new HttpClient();
+var downloader = new HuntReportDownloader(httpClient);
+var worker = new DownloadWorker(downloader, 500);
+
+var tokenSource = new CancellationTokenSource();
+worker.Run(tokenSource.Token);
+
+public class HuntReportDownloader
+{
+    private readonly HttpClient client;
+    private readonly string huntReportUrl = "https://www.bdl.lasy.gov.pl/portal/BULiGL.BDL.Reports/Map/HuntReportData?objectId=";
+    private readonly int emptyResultLength = 18;
+
+    public HuntReportDownloader(HttpClient client) => this.client = client;
+
+    public HuntReportDownloaderResult? DownloadId(int id)
+    {
+        var currentUrl = this.huntReportUrl + id;
+        var response = this.client!.GetAsync(currentUrl).Result;
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var result = response.Content.ReadAsByteArrayAsync().Result;
+        return result.Length == this.emptyResultLength ? null : new(id, result);
+    }
+}
+
+public class DownloadWorker
+{
+    private readonly string savepointPath = "savepoint.txt";
+    private readonly string resultsPath = "/Results/";
+    private readonly HuntReportDownloader downloader;
+    private readonly int waitMiliseconds;
+    private int currentId;
+
+    public DownloadWorker(HuntReportDownloader downloader, int waitMiliseconds)
+    {
+        if (File.Exists(this.savepointPath))
+        {
+            if (int.TryParse(File.ReadAllText(this.savepointPath), out var result))
+            {
+                this.currentId = result;
+            }
+        }
+        else
+        {
+            File.Create(this.savepointPath).Dispose();
+        }
+
+        this.downloader = downloader;
+        this.waitMiliseconds = waitMiliseconds;
+    }
+
+    public void Run(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            var result = this.downloader.DownloadId(this.currentId);
+            if (result != null)
+            {
+                var savePath = Path.Combine(this.resultsPath, this.currentId.ToString() + ".json");
+                using var writer = new BinaryWriter(File.Create(savePath));
+                var bytes = result.Data.AsSpan();
+                writer.Write(bytes);
+            }
+
+            this.currentId++;
+            File.WriteAllText(this.savepointPath, this.currentId.ToString());
+            Thread.Sleep(this.waitMiliseconds);
+        }
+    }
+}
+
+public record HuntReportDownloaderResult(int Id, byte[] Data);
